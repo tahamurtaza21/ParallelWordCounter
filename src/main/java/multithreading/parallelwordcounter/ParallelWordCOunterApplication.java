c8813d03f614b857461b051ff7e92a2306561662
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2025-09-19 18:23:02 +0100

    Created new project for learning multi threading

---
+++ src/main/java/multithreading/parallelwordcounter/ParallelWordCOunterApplication.java
@@ -0,0 +1,13 @@
+package multithreading.parallelwordcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelWordCOunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelWordCOunterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/multithreading/parallelwordcounter/ParallelWordCOunterApplicationTests.java
@@ -0,0 +1,13 @@
+package multithreading.parallelwordcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelWordCOunterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
